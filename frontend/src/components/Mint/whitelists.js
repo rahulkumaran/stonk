36d5b992808f5d287a8d39cae5ee1eb64be0fc08
adeeshsharma <adeeshon@gmail.com>
2021-12-07 00:09:14 +0530

    updated whitelist test  addr

--- frontend/src/components/Mint/whitelists.js
+++ frontend/src/components/Mint/whitelists.js
@@ -1,3 +1,9 @@
-const whitelist_addresses = ['0xB3CD6922415a5C561CDB83a16205A7a403aEDED3']
+const whitelist_addresses = [
+ '0xB3CD6922415a5C561CDB83a16205A7a403aEDED3',
+ '0xd4aE60d4aF6dEc4de59bf5AC280355060871Dc79',
+ '0x4E2CC19317d23680AAfE52189491D12F890c3c02',
+ '0x8922720485893029a970AEc9063F68e8Edc47AD9',
+ '0x988b2a936bD84b2c78A0eC234821A87AA64e6Aa6'
+]
 
 export const addresses = whitelist_addresses.map(addr => addr.toUpperCase());
